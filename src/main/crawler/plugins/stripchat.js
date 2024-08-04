263d5595d9705150bdb5a8e63b394878661f74b7
WLaoDuo <69497874+WLaoDuo@users.noreply.github.com>
2024-08-04 13:26:25 +0800

    Create stripchat.js
    support www.stripchat.com

---
+++ src/main/crawler/plugins/stripchat.js
@@ -0,0 +1,61 @@
+import debug from 'debug'
+
+import { request , DESKTOP_USER_AGENT } from '../base-request.js'
+import { captureError } from '../capture-error.js'
+
+import { CRAWLER_ERROR_CODE, SUCCESS_CODE } from '../../../code'
+
+const log = debug('fideo-crawler-stripchat')
+
+function getRoomIdByUrl(url) {
+  return new URL(url).pathname.split('/')[1]
+}
+
+async function baseGetStripchatLiveUrlsPlugin(roomUrl, others = {}) {
+  const modelName = getRoomIdByUrl(roomUrl)
+  const { proxy, cookie } = others
+
+  log('modelName:', modelName, 'cookie:', cookie, 'proxy:', proxy)
+
+  const response = (
+    await request(`https://zh.stripchat.com/api/front/v2/models/username/${modelName}/chat`, {
+      method: 'get',
+      proxy,
+      headers: {
+        // cookie,
+        // 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+        'User-Agent': DESKTOP_USER_AGENT ,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+      	"Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+      	"Accept-Encoding": "gzip, deflate",
+      	"Upgrade-Insecure-Requests": "1",
+      	"Sec-Fetch-Dest": "document",
+      	"Sec-Fetch-Mode": "navigate",
+      	"Sec-Fetch-Site": "none",
+      	"Sec-Fetch-User": "?1",
+      	"Te": "trailers"
+      	// "Connection": "close"
+      }
+    })
+  ).data
+
+  // const scriptReg = /<script\b[^>]*>([\s\S]*?)<\/script>/gi
+  // const matches = htmlContent.match(scriptReg)
+  const modelID = response.messages.0.modelId
+  const errMessage=response.errmsg
+  
+  const url = `https://edge-hls.doppiocdn.com/hls/${roomId}/master/${roomId}_auto.m3u8?playlistType=lowLatency`
+  
+
+
+
+
+
+  
+  return {
+    code: SUCCESS_CODE,
+    liveUrls: [url]
+  }
+}
+
+export const getStripchatLiveUrlsPlugin = captureError(baseGetStripchatLiveUrlsPlugin)
