142f399bad59379977b010d6ed306c745fe51c4f
WLaoDuo <69497874+WLaoDuo@users.noreply.github.com>
2024-08-04 12:56:40 +0800

    Update index.js
    add stripchat

--- src/main/crawler/index.js
+++ src/main/crawler/index.js
@@ -17,6 +17,7 @@ import { getYYLiveUrlsPlugin } from './plugins/yy'
 import { getJDLiveUrlsPlugin } from './plugins/jd'
 import { getMomoLiveUrlsPlugin } from './plugins/momo'
 import { getShiGuangLiveUrlsPlugin } from './plugins/shiguang'
+import { getStripchatLiveUrlsPlugin } from './plugins/stripchat'
 
 import { CRAWLER_ERROR_CODE } from '../../code'
 
@@ -65,6 +66,9 @@ const hostnameToPlatformCrawlerFnMap = {
   'web.immomo.com': getMomoLiveUrlsPlugin,
 
   'wap.rengzu.com': getShiGuangLiveUrlsPlugin
+
+  'zh.stripchat.com' : getStripchatLiveUrlsPlugin
+  'www.stripchat.com': getStripchatLiveUrlsPlugin
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
