fe089d6dd40be182541bb227d4e93b6e497f3f61
WLaoDuo <69497874+WLaoDuo@users.noreply.github.com>
2024-08-05 16:18:25 +0800

    update stripchat

--- src/main/crawler/plugins/stripchat.js
+++ src/main/crawler/plugins/stripchat.js
@@ -11,8 +11,6 @@ function getmodelIdByUrl(url) {
   return new URL(url).pathname.split('/')[1]
 }
 
-
-
 async function baseGetStripchatLiveUrlsPlugin(roomUrl, others = {}) {
   const modelName = getmodelIdByUrl(roomUrl)
   const { proxy, cookie } = others
@@ -20,7 +18,7 @@ async function baseGetStripchatLiveUrlsPlugin(roomUrl, others = {}) {
   log('modelName:', modelName, 'cookie:', cookie, 'proxy:', proxy)
   
 //////////////// get_modelId(name) ////////////////////////////////////////////////////////////
-  const response1 = (
+  let response = (
     await request(`https://zh.stripchat.com/api/front/v2/models/username/${modelName}/chat`, {
       method: 'get',
       proxy,
@@ -41,32 +39,27 @@ async function baseGetStripchatLiveUrlsPlugin(roomUrl, others = {}) {
       }
     })
   )
-  console.log(response1.status)
+  
   let modelId ="false"
 
-  if ( response1.status==200 ) {
-    if(response1.data.messages.length > 2) {
-      modelId = response1.data.messages[0].modelId
+
+  if ( response.status==200 ) {
+    if(response.data.messages.length > 0) {
+      modelId = response.data.messages[0].modelId
     } else {
-      console.log(response1.data)
-      modelId= 'OffLine'
+      // console.log(response.data.messages.length,response.data)
+      modelId= 'OffLine' //{"messages":[]}无数据，代表主播下线，直播间未开播
       return { code: 0}
     }
   }
-//////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// get_M3u8(modelId) /////////////////////////////////////////////////////////////////////
 
+  console.log(modelName,"modelID:",modelId)
 
-
-
-
-
-
-  console.log("modelID:",modelId)
-
-  const response2 = (
+  response = (
     await request(`https://edge-hls.doppiocdn.com/hls/${modelId}/master/${modelId}_auto.m3u8?playlistType=lowLatency`, {
       method: 'get',
-      proxy,
+      proxy, //edge-hls.doppiocdn.com可以直接访问，不必挂代理
       headers: {
         // cookie,
         // 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
@@ -75,16 +68,33 @@ async function baseGetStripchatLiveUrlsPlugin(roomUrl, others = {}) {
     })
   )
   
-  // console.log(response2.data,response2.status)
   const scriptReg = /(https:\/\/[\w\-\.]+\/hls\/[\d]+\/[\d\_p]+\.m3u8\?playlistType=lowLatency)/
-  const matches = response2.data.match(scriptReg)
-  if (response2.status==200 && matches && matches.length > 0) {
-    const url=matches[0]
-    console.log(url)
+  const matches = response.data.match(scriptReg)
+  if (response.status==200 && matches && matches.length > 0) {
+    let url=matches[0]
+    console.log(modelName,"m3u8:",url)
 
-    return {
-      code: SUCCESS_CODE,
-      liveUrls: [url]
+//////////////////////////// test_m3u8(m3u8) ///////////////////////////////////////////////////////////////////////////////
+    response = (
+      await request(url, {
+        method: 'get',
+        proxy,
+        headers: {
+          // cookie,
+          // 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+          'User-Agent': DESKTOP_USER_AGENT ,
+        }
+      })
+    )
+    console.log(modelName,"response 3:",response.status)
+    if (response.status==200){
+      return {
+        code: SUCCESS_CODE,
+        liveUrls: [url]
+      }
+    }
+    if (response.status==403){ //m3u8 403主播开启私人票房，普通用户看不见，=下线状态，得进入 《监控》状态
+      return{ code:0 }
     }
   }
 }
